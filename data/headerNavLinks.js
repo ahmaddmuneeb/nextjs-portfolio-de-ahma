e662c35d747a2f4eb5d4573339be62ac94480467
Muneeb Ahmad <ahmaddmuneeb786@gmail.com>
2023-06-20 07:58:44 +0500

    updated routes

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,15 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/', title: 'Posts' },
+  { href: '/blog', title: 'Posts' },
   { href: '/projects', title: 'Projects' },
-  { href: '/', title: 'About' },
+  { href: '/about', title: 'About' },
 ];
-
-// const headerNavLinks = [
-//   { href: '/', title: 'Home' },
-//   { href: '/blog', title: 'Posts' },
-//   { href: '/projects', title: 'Projects' },
-//   { href: '/about', title: 'About' },
-// ];
 
 export default headerNavLinks;
